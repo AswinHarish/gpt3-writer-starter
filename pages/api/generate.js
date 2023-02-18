e970742d6941eda36a797117c3cd9b03ff65f4e1
Aswin Harish <56496128+AswinHarish@users.noreply.github.com>
2023-02-18 16:53:36 +0530

    Gpt writer final

---
+++ pages/api/generate.js
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+`
+Generate a tweet thread about topic below. Include the history, inventor, tech behind it, the working, how it is useful, and all other info.  Generate threads somewhere between 5-8 as you like
+Topic: 
+`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.6,
+    max_tokens: 600,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
